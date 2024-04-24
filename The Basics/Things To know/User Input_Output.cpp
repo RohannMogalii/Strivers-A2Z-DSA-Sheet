2e7fa1ecaa84436f21ddf78a3c7e5718993d37b5
Rohan Mogali <141552712+RohannMogalii@users.noreply.github.com>
2024-04-24 12:02:56 +0530

    Add files via upload

--- The Basics/Things To know/User Input_Output.cpp
+++ The Basics/Things To know/User Input_Output.cpp
@@ -1,1 +1,23 @@
-ejfnmc
+// https://www.codingninjas.com/studio/problems/find-character-case_58513?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(char ch){
+	if(ch>='A' && ch<='Z'){
+		cout<<1;
+	}else if(ch>='a' && ch<='z'){
+		cout<<0;
+	}else{
+		cout<<-1;
+	}
+}
+int main(){
+	char ch;
+	cin>>ch;
+
+	solve(ch);
+
+	return 0;
+}
